bc2438b2de8428ecc3d7543076b997d547c0a1c8
longjichun <longhtml5@163.com>
2017-04-01 22:14:34 +0800

    c

--- dataset/treeDataSet.js
+++ dataset/treeDataSet.js
@@ -16,6 +16,9 @@
 		//用于渲染界面的数据
 		this.renderData = [];
 
+		//分页前的数据
+		this.beforeLimitData = []
+
 		//包括叶子借点在内的所有hash型数据 以id为名
 		this.trunkNodeId = {};
 
@@ -93,10 +96,63 @@
 		this.trunkNodeId = trunkNodeId;
 		this.trunkNodePid = trunkNodePid;
 		this.topTrunkNodeId = topTrunkNodeId;
+		self.beforeLimitData = topTrunkNodeId;
 	}
-	$.prototype.loadData = function(config){
+	/**
+	 * [exChangeNode 展开与收缩节点的方法]
+	 * @param  {[type]} id  要展开或收缩节点的id
+	 * @param  {[type]} ind 节点在已存在的数据中的位置
+	 * @return {[type]}     [description]
+	 */
 
+	function doSort(){
+		var self = this;
+		self.sortCache = toSort(self.topTrunkNodeId,sorts);
+		self.filterCache = self.sortCache.slice(0);
 	}
+	function doFilter() {
+		var self = this;
+		self.filterCache = toFilter(self.sortCache,conditions);
+	}
+	function doLimit() {
+		var self = this;
+		self.limitData = toLimit(self.filterCache,limitConf);
+	}
+	function getChild(data){
+		var self = this;
+		return self.renderData;
+	}
+	function toSort(sorts) {
+		var self = this;
+		if(Array.isArray(sorts)) {
+			var sortStrand = sorts.reverse();
+		} else {
+			console.error("排序指标为数组");
+		}
+		var tops = self.cacheData.topTrunkNodeId?self.cacheData.topTrunkNodeId:self.topTrunkNodeId; 
+		tops.sort(function(x,y){
+			return self.sortFn(x,y,sortStrand);
+		});
+		//保存每次排序、过滤后的数据
+		self.cacheData.topTrunkNodeId = tops.slice(0);
+		self.sortCache.topTrunkNodeId = tops.slice(0);
+		var _tops = tops.slice(0);
+
+		var selfConfPage = self.loadConfig.page;
+		//有缓存就从缓存拿
+		var curTops = _tops.splice(selfConfPage.start,selfConfPage.len);
+		self.renderData = self.getExpandList(curTops,false);
+	}
+	$.prototype.loadData = function(config,cb){
+		var self = this;
+		doSort().doFilter().doLimit();
+		if(typeof cb != "function") {
+			return self.renderData;
+		} else {
+			cb(self.renderData);
+		}
+	}
+
 	if( typeof exports !== 'undefined' && typeof module !== 'undefined' && module.exports){
 		exports.$ = module.exports = $;
 	} else {
